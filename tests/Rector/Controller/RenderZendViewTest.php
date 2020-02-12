fca8174800ea85fbe6d481406a3c007c2cd8e24d
Luca Feiser <luca.feiser@gmail.com>
2020-02-12 13:47:12 +0100

    RenderZenView Testcase

---
+++ tests/Rector/Controller/RenderZendViewTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Complex\Zend2RocketRector\Tests\Rector\Controller;
+
+use Complex\Zend2RocketRector\Rector\Controller\ActionCamelCaseToSnakeCase;
+use Iterator;
+use Complex\Zend2RocketRector\Tests\AbstractRectorWithConfigTestCase;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+class RenderZendViewTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideDataForTest()
+     *
+     * @param string $file
+     */
+    public function test(string $file): void
+    {
+        $this->doTestFile($file);
+    }
+
+    public function provideDataForTest(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/RenderZendViewFixture.php.inc'];
+    }
+
+    protected function getRectorClass(): string
+    {
+        return RenderZendViewTest::class;
+    }
+}
