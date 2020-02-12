8b14df65aa768c95d1a8ad58934af46b5f01afb8
Luca Feiser <luca.feiser@gmail.com>
2020-02-12 11:40:32 +0100

    crrected return type annotation

--- tests/Rector/Controller/ActionCamelCaseToSnakeCaseTest.php
+++ tests/Rector/Controller/ActionCamelCaseToSnakeCaseTest.php
@@ -2,10 +2,12 @@
 
 namespace Complex\Zend2RocketRector\Tests\Rector\Controller;
 
+use Complex\Zend2RocketRector\Rector\Controller\ActionCamelCaseToSnakeCase;
 use Iterator;
 use Complex\Zend2RocketRector\Tests\AbstractRectorWithConfigTestCase;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 
-class ActionCamelCaseToSnakeCaseTest extends AbstractRectorWithConfigTestCase
+class ActionCamelCaseToSnakeCaseTest extends AbstractRectorTestCase
 {
     /**
      * @dataProvider provideDataForTest()
@@ -20,5 +22,10 @@ class ActionCamelCaseToSnakeCaseTest extends AbstractRectorWithConfigTestCase
     public function provideDataForTest(): Iterator
     {
         yield [__DIR__ . '/Fixture/ActionCamelCaseToSnakeCaseFixture.php.inc'];
+    }
+
+    protected function getRectorClass(): string
+    {
+        return ActionCamelCaseToSnakeCase::class;
     }
 }
